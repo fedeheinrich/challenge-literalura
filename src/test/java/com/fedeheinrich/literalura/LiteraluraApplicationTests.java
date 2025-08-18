601383865834d77a2550cbf51fcce952ea1c1727
Federico Heinrich <fedeheinrich199@gmail.com>
2025-08-18 00:07:17 -0300

    Subiendo Proyecto Spring Initializr al repo

---
+++ src/test/java/com/fedeheinrich/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedeheinrich.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
